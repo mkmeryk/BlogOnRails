41d649e3fb3a9c54d8ea44870889eea2767d5f22
mkmeryk <inq@meryk.ca>
2022-03-14 19:16:59 -0700

    Posts Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Post.destroy_all
+
+50.times do
+
+    created_at = Faker::Date.backward(days: 365 * 5)
+
+    Post.create(
+        title: Faker::Hacker.say_something_smart,
+        body: Faker::Lorem.characters(number: 51)
+    )
+
+end
+
+posts = Post.all
+puts "generated #{posts.count} posts"
