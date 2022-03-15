6e3230a950117787060bc5a87d93baa2245ca1e5
mkmeryk <inq@meryk.ca>
2022-03-15 01:11:02 -0700

    Posts Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update , :show, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if@post.save
+      flash[:notice] = "Post created!"
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    # @post = Post.find params[:id]
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def edit 
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+
+  #destroy and update does not work
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
+
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :posts
+
 end
